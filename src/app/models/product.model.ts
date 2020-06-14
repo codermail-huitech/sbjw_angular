443f1645951d09401f367b17b68f34f169e7fc06
sukanta hui <huisukanta77@gmail.com>
2020-06-14 11:53:31 +0530

    Merge remote-tracking branch 'origin/master' product style changed # Conflicts: #	src/app/pages/product/product.component.ts

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -7,6 +7,8 @@ export class Product{
   product_name: string;
   product_category_id: number;
   price_code_id: number;
+  price_code_name?: string;
+  category_name?: string;
   constructor(){
 
   }

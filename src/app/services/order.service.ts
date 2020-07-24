e20719b27ad646d86086b893757f7a82876b90bd
RiyaGuha1234 <donaguhachoudhury@gmail.com>
2020-07-23 21:07:18 -0700

    orderform error solved partially

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -124,11 +124,13 @@ export class OrderService {
 
   saveOrder(){
     // tslint:disable-next-line:max-line-length
+       console.log(this.orderMaster);
        return this.http.post<OrderResponseData>( GlobalVariable.BASE_API_URL + '/orders', {master: this.orderMaster, details: this.orderDetails})
          .pipe(catchError(this._serverError), tap(((response: {success: number, data: OrderMaster}) => {
-          if(this.orderMaster.id==null) {
+          if(this.orderMaster.id===null) {
             this.orderMasterData.unshift(response.data);
           }
+          // console.log(this.orderMaster);
           
           this.orderSub.next([...this.orderMasterData]);
         })));

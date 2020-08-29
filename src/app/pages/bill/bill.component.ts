f9ac7bf1ed909be5b3b1551e2d70905441a39c0e
codermail-huitech <codermail.huitech@gmail.com>
2020-08-29 16:01:28 +0530

    order master number shown in create bill tab

--- src/app/pages/bill/bill.component.ts
+++ src/app/pages/bill/bill.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {BillService} from "../../services/bill.service";
 import {FinishedJobs} from "../../models/finishedJobs";
+import {Material} from "../../models/material.model";
+import {OrderDetail} from "../../models/orderDetail.model";
 
 @Component({
   selector: 'app-bill',
@@ -9,16 +11,34 @@ import {FinishedJobs} from "../../models/finishedJobs";
 })
 export class BillComponent implements OnInit {
   finshedJobs : FinishedJobs[] = [];
+  showDetails = false;
+  orderDetails: OrderDetail[];
 
   constructor(private  billService : BillService) { }
 
   ngOnInit(): void {
+    this.showDetails = false;
 
     this.billService.getFinishedJobsSubUpdateListener().subscribe((finishedJobs)=>{
        this.finshedJobs=finishedJobs;
-       console.log(this.finshedJobs);
     })
 
+    this.billService.getOrderDetailsSubUpdateListener()
+      .subscribe((details: OrderDetail[]) => {
+        this.orderDetails = details;
+    });
+
+  }
+
+  getDetails(data){
+    this.showDetails = true;
+    let data1 = this.billService.getDetails(data.order_master_id);
+    console.log('data1');
+    console.log(data1);
+  }
+
+  changeShowDetails(){
+    this.showDetails = false;
   }
 
 }
--- src/app/services/bill.service.ts
+++ src/app/services/bill.service.ts
@@ -4,6 +4,9 @@ import {FinishedJobs} from "../models/finishedJobs";
 import {GlobalVariable} from "../shared/global";
 import {Karigarh} from "../models/karigarh.model";
 import {Subject} from "rxjs";
+import {Product} from "../models/product.model";
+import {ProductResponseData} from "./product.service";
+import {OrderDetail} from "../models/orderDetail.model";
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +14,16 @@ import {Subject} from "rxjs";
 export class BillService {
 
   finshedJobs : FinishedJobs[] = [];
+  orderDetails : OrderDetail[] = [];
   private finishedJobsSub = new Subject<FinishedJobs[]>();
+  private orderDetailsSub = new Subject<OrderDetail[]>();
 
   getFinishedJobsSubUpdateListener(){
     return this.finishedJobsSub.asObservable();
+  }
+
+  getOrderDetailsSubUpdateListener(){
+    return this.orderDetailsSub.asObservable();
   }
 
   constructor(private http: HttpClient) {
@@ -24,7 +33,14 @@ export class BillService {
         this.finshedJobs = data;
         this.finishedJobsSub.next([...this.finshedJobs]);
       });
+  }
 
-
+  getDetails(data){
+    return this.http.post<ProductResponseData>('http://127.0.0.1:8000/api/fetchingDetails', data)
+      .subscribe((response: {success: number, data: OrderDetail[]})  => {
+        const {data} = response;
+        this.orderDetails = data;
+        this.orderDetailsSub.next([...this.orderDetails]);
+      });
   }
 }

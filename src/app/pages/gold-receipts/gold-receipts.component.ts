f5b4061c8dbec7df213840847a86c54dbeb6f1f9
RiyaGuha1234 <donaguhachoudhury@gmail.com>
2020-10-06 10:14:10 +0530

    gold received done

--- src/app/pages/gold-receipts/gold-receipts.component.ts
+++ src/app/pages/gold-receipts/gold-receipts.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import {BillService} from "../../services/bill.service";
 import {GoldReceiptService} from "../../services/gold-receipt.service";
 import {FinishedJobs} from "../../models/finishedJobs";
-import {FormGroup} from "@angular/forms";
+import {FormControl, FormGroup} from "@angular/forms";
 import {DatePipe} from "@angular/common";
+import Swal from "sweetalert2";
+import {GoldReceipt} from "../../models/goldReceipt.model";
 
 @Component({
   selector: 'app-gold-receipts',
@@ -12,14 +14,25 @@ import {DatePipe} from "@angular/common";
 })
 export class GoldReceiptsComponent implements OnInit {
 
-  completedBillList : FinishedJobs[];
+  completedBillList : GoldReceipt[];
   goldReceivedForm : FormGroup;
 
   minDate = new Date(2010, 11, 2);
   maxDate = new Date(2021, 3, 2);
   pipe = new DatePipe('en-US');
 
-  constructor(private goldReceiptService : GoldReceiptService) { }
+  public searchTerm: string;
+  filter = new FormControl('');
+  page: number;
+  pageSize: number;
+  p = 1;
+
+  constructor(private goldReceiptService : GoldReceiptService) {
+    this.completedBillList = this.goldReceiptService.getCompletedBills();
+
+    this.page = 1;
+    this.pageSize = 15;
+  }
 
   ngOnInit(): void {
     this.goldReceiptService.getCompletedBillUpdateListener().subscribe((response)=>{
@@ -29,14 +42,29 @@ export class GoldReceiptsComponent implements OnInit {
   }
 
   fillGoldReceivedForm(data){
-    this.goldReceivedForm.patchValue({customer_id : data.customer_id , agent_id : data.agent_id});
+    this.goldReceivedForm.patchValue({bill_master_id : data.id ,customer_id : data.customer_id , agent_id : data.agent_id ,customer_name : data.customer_name,agent_name : data.agent_name});
 
 
   }
 
   onSubmit(){
+    console.log(this.goldReceivedForm.value);
     this.goldReceivedForm.value.received_date = this.pipe.transform(this.goldReceivedForm.value.received_date, 'yyyy-MM-dd');
-    this.goldReceiptService.SaveReceivedGold(this.goldReceivedForm.value).subscribe();
+    this.goldReceiptService.SaveReceivedGold(this.goldReceivedForm.value).subscribe((response)=>{
+      if(response.success === 1)
+      {
+        Swal.fire(
+          'Done!',
+          'Received Gold Submitted',
+          'success'
+        );
+        this.goldReceiptService.getCompletedBillUpdateListener().subscribe((response)=>{
+          this.completedBillList = response;
+        });
+        this.goldReceivedForm.reset();
+
+      }
+    });
 
   }
 
--- src/app/services/gold-receipt.service.ts
+++ src/app/services/gold-receipt.service.ts
@@ -6,14 +6,15 @@ import {JobMaster} from "../models/jobMaster.model";
 import {Subject} from "rxjs";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {catchError, tap} from 'rxjs/operators';
+import {GoldReceipt} from "../models/goldReceipt.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoldReceiptService {
 
-  completedBillList : FinishedJobs[]=[];
-  private completedBillSub = new Subject<FinishedJobs[]>();
+  completedBillList : GoldReceipt[]=[];
+  private completedBillSub = new Subject<GoldReceipt[]>();
 
   goldReceivedForm : FormGroup;
 
@@ -26,14 +27,17 @@ export class GoldReceiptService {
 
     this.goldReceivedForm = new FormGroup({
       id : new FormControl(null),
+      customer_name: new FormControl({value : null, disabled: true}),
+      agent_name: new FormControl({value : null, disabled: true}),
       received_date : new FormControl(null, [Validators.required]),
       customer_id : new FormControl(null, [Validators.required]),
       agent_id : new FormControl( null, [Validators.required]),
+      bill_master_id : new FormControl( null, [Validators.required]),
       gold_received : new FormControl( null, [Validators.required])
     });
 
     this.http.get(GlobalVariable.BASE_API_URL + '/getCompletedBills')
-      .subscribe((response: {success: number, data: FinishedJobs[]}) => {
+      .subscribe((response: {success: number, data: GoldReceipt[]}) => {
         const {data} = response;
         this.completedBillList = data;
         this.completedBillSub.next([...this.completedBillList]);
@@ -43,8 +47,15 @@ export class GoldReceiptService {
 
     this.goldReceivedForm = item;
     return this.http.post(GlobalVariable.BASE_API_URL + '/SaveReceivedGold',this.goldReceivedForm)
-      .pipe((tap((response)=>{
-
+      .pipe((tap((response: {success : number , data : GoldReceipt})=>{
+          let index = this.completedBillList.findIndex(x=>x.id===item.bill_master_id);
+          this.completedBillList[index].gold_received = response.data.gold_received;
+          // console.log('test');
+          // console.log(this.completedBillList);
+          this.completedBillSub.next([...this.completedBillList]);
       })));
+  }
+  getCompletedBills(){
+    return  [...this.completedBillList];
   }
 }
